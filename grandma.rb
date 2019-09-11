4d0c8d64c6cfe31a420c791ac5066c8bf5acab23
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-11 15:21:23 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,14 @@
+def speak_to_grandma(greeting)
+  if greeting == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+    elsif greeting == greeting.upcase
+    return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!".upcase
+  end
+end
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
